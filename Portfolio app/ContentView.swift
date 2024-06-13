ca8ebc8ab245d7951db8240e78e349f8631dee49
Ayush Rajpal <ayushrajpal2021@gmail.com>
2024-06-13 19:05:07 +0530

    add basic ui for the app

--- Portfolio app/ContentView.swift
+++ Portfolio app/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "cloud.fill")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Ayush!")
+        
+        ZStack {
+            Color(.brandWhite)
+                .ignoresSafeArea(.all)
+            VStack{
+                Image("ayush")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150,height: 150)
+                    .clipShape(Circle())
+                    .overlay{
+                        Circle().stroke(Color(.brandOrange), lineWidth: 4)
+                    }
+                Text("Ayush Rajpal")
+                    .foregroundColor(Color(.brandOrange))
+                    .font(.system(size: 40))
+                    .font(.headline)
+                    .bold()
+                Text("iOS Devloper")
+                    .foregroundColor(Color(.brandOrange))
+                    .font(.system(size: 25))
+                Divider()
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                    .frame(height: 50)
+                    .foregroundColor(.brandLightGreay)
+            }
         }
-        .padding()
+        
     }
 }
 
